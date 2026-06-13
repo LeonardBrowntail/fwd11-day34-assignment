c9da378abff981cbdbea94423c14c9f3329c73ec
D_037 <d.leo037.study@gmail.com>
2026-06-13 23:29:14 +0700

    ROUTE: Created route for API resources

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,11 @@
 <?php
 
+use App\Http\Controllers\CourseCategoryController;
+use App\Http\Controllers\CourseController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::apiResources([
+  'courses' => CourseController::class,
+  'courseCategories' => CourseCategoryController::class
+]);
