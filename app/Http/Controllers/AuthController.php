6c6e0cd774f03aa9fa798fe13adb60f9a319d040
D_037 <d.leo037.study@gmail.com>
2026-06-20 02:52:41 +0700

    auth: added authentication controller along with its form requests

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\ApiResponse;
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Hash;
+
+class AuthController extends Controller
+{
+    use ApiResponse;
+    public function register(RegisterRequest $request) {
+        if (isset($request->validator) && $request->validator->fails()) {
+            return $this->errorResponse($request->validator->errors());
+        }
+        User::create($request->validated());
+        return $this->generalResponse(true, "Berhasil mendaftar", 200);
+    }
+
+    public function login(LoginRequest $request) {
+        if (isset($request->validator) && $request->validator->fails()){
+            return $this->errorResponse($request->validator->errors());
+        }
+
+        // check username
+        $user = User::where(['email'], $request->validated('email'))->first();
+        // check password
+        $validPass = Hash::check($request->validated('password'), $user->password);
+        
+        if (!$user || !$validPass) {
+            return $this->generalResponse(false, "Email atau password salah", 401);
+        }
+
+        $token = $user->createToken('auth-token')->plainTextToken;
+        return $this->generalResponse(true, "Berhasil login", 200, $token);
+    }
+
+    public function logout(Request $request) {
+        $request->user()->currentAccessToken()->delete();
+        return $this->generalResponse(true, "Berhasil logout", 200);
+    }
+}
---
+++ app/Http/Requests/RegisterRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator as Val;
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterRequest extends FormRequest
+{
+    public Val $validator = null;
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['reqired', 'string', 'max:64'],
+            'email' => ['required', 'email', 'unique:users,email'],
+            'role' => ['required', 'string', 'in:student,instructor'],
+            'password' => ['required', 'password','min:8', 'confirmed']
+        ];
+    }
+
+    protected function failedValidation(Val $validator) {
+        $this->validator = $validator;
+    }
+}
