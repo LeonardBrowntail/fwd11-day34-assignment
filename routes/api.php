64f8a1e457a8991af2d683b2d83aded7cdfb5ff9
D_037 <d.leo037.study@gmail.com>
2026-06-20 02:51:35 +0700

    auth: added authentication routes and route protection with laravel sanctum

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,24 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CourseCategoryController;
 use App\Http\Controllers\CourseController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResources([
-  'courses' => CourseController::class,
-  'categories' => CourseCategoryController::class
+// Protected routes
+Route::middleware('auth:sanctum')->prefix('v2/')->group(fn()=>[
+  Route::apiResource('courses', CourseController::class)->except('index', 'show'),
+  Route::apiResource('categories', CourseCategoryController::class)->except('index', 'show'),
+  Route::post('logout', [AuthController::class, 'logout'])
+]);
+
+// public routes
+Route::prefix('v2/')->group(fn()=>[
+  Route::apiResource('courses', CourseController::class)->only('index', 'show'),
+  Route::apiResource('categories', CourseCategoryController::class)->only('index', 'show')
+]);
+
+Route::prefix('auth/')->group(fn() => [
+  Route::post('register', [AuthController::class, 'register']),
+  Route::post('login', [AuthController::class, 'login'])
 ]);
