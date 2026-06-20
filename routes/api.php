c162573f04b6e608e47a2ecb2a0d83672ba91f9a
D_037 <d.leo037.study@gmail.com>
2026-06-20 15:07:01 +0700

    auth: updated routes

--- routes/api.php
+++ routes/api.php
@@ -5,20 +5,23 @@ use App\Http\Controllers\CourseCategoryController;
 use App\Http\Controllers\CourseController;
 use Illuminate\Support\Facades\Route;
 
-// Protected routes
+// Protected API Routes
 Route::middleware('auth:sanctum')->prefix('v2/')->group(fn()=>[
-  Route::apiResource('courses', CourseController::class)->except('index', 'show'),
-  Route::apiResource('categories', CourseCategoryController::class)->except('index', 'show'),
-  Route::post('logout', [AuthController::class, 'logout'])
+  Route::apiResource('courses', CourseController::class)->only('store', 'update', 'destroy'),
+  Route::apiResource('categories', CourseCategoryController::class)->only('store', 'update', 'destroy')
 ]);
-
-// public routes
+  
+// Public API Routes
 Route::prefix('v2/')->group(fn()=>[
   Route::apiResource('courses', CourseController::class)->only('index', 'show'),
   Route::apiResource('categories', CourseCategoryController::class)->only('index', 'show')
 ]);
 
+// Authentication Routes
 Route::prefix('auth/')->group(fn() => [
+  // Public
   Route::post('register', [AuthController::class, 'register']),
-  Route::post('login', [AuthController::class, 'login'])
+  Route::post('login', [AuthController::class, 'login']),
+  // Protected
+  Route::middleware('auth:sanctum')->post('logout', [AuthController::class, 'logout'])
 ]);
